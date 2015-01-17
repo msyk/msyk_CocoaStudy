4cc5222d4d6567853067f34d2dc6718e7941df3c
msyk <nii@msyk.net>
2015-01-18 00:52:44 +0900

    Add 7-_DemoAVF

---
+++ 70_DemoAVF/DemoAVF/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DemoAVF
+//
+//  Created by 新居雅行 on 2015/01/17.
+//  Copyright (c) 2015年 msyk.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 70_DemoAVF/DemoAVF/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoAVF
+//
+//  Created by 新居雅行 on 2015/01/17.
+//  Copyright (c) 2015年 msyk.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
